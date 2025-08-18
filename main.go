098b718d538a523db32bf5233c9c5c7819a672dd
needthisforctf <111290117+needthisforctf@users.noreply.github.com>
2025-08-18 16:52:53 +0400

    Update main.go

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 	log.Printf("XRay torrent-blocker: %s", Version)
 	log.Printf("Service started on %s", config.Hostname)
 	log.Printf("Version 1.0.1")
-	log.Printf(config.IgnoreEmail)
+	log.Printf(fmt.Sprintf("%t", config.IgnoreEmail))
 
 	utils.InitConntrackManager()
 
